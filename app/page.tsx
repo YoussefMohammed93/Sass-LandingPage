2a253da10725b5c396277fed7c0671148abe566b
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-05-25 08:55:54 +0300

    Icons Grid Section Added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FeaturesSectionOne } from "@/components/features-section-one";
 import { FeaturesSectionTwo } from "@/components/features-section-two";
 import { CalloutBlockSection } from "@/components/callout-block-section";
 import { FeaturesSectionThree } from "@/components/features-section-three";
+import { IconsGridSection } from "@/components/icons-grid-section";
 
 export default function LandingPage() {
   return (
@@ -15,6 +16,7 @@ export default function LandingPage() {
         <FeaturesSectionTwo />
         <CalloutBlockSection />
         <FeaturesSectionThree />
+        <IconsGridSection />
       </main>
     </>
   );
