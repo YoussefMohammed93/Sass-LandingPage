dd67673c1409e3cf973984b2a38efdfa991d6df4
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-05-25 16:09:32 +0300

    Pricing Callout Section Added

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,7 @@
 import { PricingHero } from "@/components/pricing-hero";
 import { PricingPlans } from "@/components/pricing-plans";
 import { PricingIncludedSection } from "@/components/pricing-included-section";
+import { CalloutBlockSectionPricingOne } from "@/components/callout-block-section-pricing-one";
 
 export default function PricingPage() {
   return (
@@ -8,6 +9,7 @@ export default function PricingPage() {
       <PricingHero />
       <PricingPlans />
       <PricingIncludedSection />
+      <CalloutBlockSectionPricingOne />
     </main>
   );
 }
